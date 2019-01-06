998a13929916a19ca7b9c8f56688e7dae842e780
Michiel Kamphuis <mfk@kamphuis.eu>
2019-01-06 20:29:43 +0100

    add option to use smart_run

--- src/api/create.ts
+++ src/api/create.ts
@@ -2,20 +2,42 @@ import { writeFileSync } from 'fs'
 
 const defaultInstapyPath = `${__dirname}/../../InstaPy`
 
-const importInstapy = 'from instapy import InstaPy'
+const importInstapy =
+  [ 'from instapy import InstaPy'
+  , 'from instapy.util import smart_run'
+  ]
+
+type Path = string
+type PythonMethod = string
+
+const takeSessionName =
+  (configLine: PythonMethod) =>
+    configLine.split('.')[0]
+
+type MakeSmartRun =
+  (configLines: PythonMethod[]) =>
+     PythonMethod[]
+const makeSmartRun: MakeSmartRun =
+  (configLines) =>
+    [ `with smart_run(${takeSessionName(configLines[0])}`
+    , ...configLines.slice(0, -1)
+    ]
 
 type setupCreate =
-  (projectPath?: string) =>
-    (configLines: string[], write?: boolean, out?: string) =>
+  (projectPath?: Path, smartRun?: boolean) =>
+    (init:PythonMethod, configLines: PythonMethod[], write?: boolean, out?: string) =>
       string
 
 export const setupCreate: setupCreate =
-  (projectPath = defaultInstapyPath) =>
-    (configLines, write = false, out = 'docker_quickstart.py') => {
+  (projectPath = defaultInstapyPath, smartRun = false) =>
+    (init, configLines, write = false, out = 'docker_quickstart.py') => {
       const config =
-        [ importInstapy
+        [ ...importInstapy
         , '\n'
-        , ...configLines
+        , init
+        , ...smartRun
+          ? makeSmartRun(configLines)
+          : configLines
         ].join('\n') + '\n'
 
       if(write) {
